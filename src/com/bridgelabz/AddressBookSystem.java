4e7d50a0906888ec206fc8985238b3bcffe94fce
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-16 22:19:29 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+/*
+ * Purpose - To create an Address Book System
+ * @author Utkarsh Mishra
+ */
+public class AddressBookSystem {
+    /*
+    Simply creating a method to print Welcome Message
+     */
+    static void welcome(){
+        System.out.println("Welcome to Address Book System Program");
+    }
+
+    public static void main(String[] args) {
+        welcome();
+    }
+}
